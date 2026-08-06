323e717b7d06476013b53f24b53ff1ab457409eb
sa.jadsonlee <sa.jadsonlee@gmail.com>
2026-08-05 21:11:38 -0300

    chore(database): configura Sequelize CLI

---
+++ backend/config/config.cjs
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: 'mysql',
+    timezone: '+00:00',
+    logging: false
+  }
+};
